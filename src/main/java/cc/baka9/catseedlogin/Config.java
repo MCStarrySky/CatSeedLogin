df84526fbe6cfcdbcfbd20cb5229e8c2c4d3140b
xiaozhangup <xiaozhangshiw@163.com>
2022-03-08 00:38:19 +0800

    移除语言文件 全部移动为Title

--- src/main/java/cc/baka9/catseedlogin/Config.java
+++ src/main/java/cc/baka9/catseedlogin/Config.java
@@ -6,6 +6,7 @@ import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.io.*;
 import java.nio.charset.Charset;
